92113b87eb6850d14e116a522acfb23fb2fa2a57
luphung <165619776+luphung@users.noreply.github.com>
2024-10-01 18:34:07 +0000

    Provided file.

---
+++ myfirstfolder/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  char hello[] = "Hello!"; 
+  char hello2[] = { 72, 101, 108, 108, 111, 33, 0 };
+
+  puts(hello);
+  puts(hello2);
+  printf("Hello, world!\n");
+  printf("This is my first edit of the provided c file\n");
+
+  return 0;
+}
